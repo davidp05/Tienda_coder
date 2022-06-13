99c57fbd87760c14891e50548bec9581c5e53e50
david.perren <david.perren05@gmail.com>
2022-06-13 19:58:28 -0300

    Se avanzó con el js

--- index.js
+++ index.js
@@ -82,11 +82,115 @@ class UI {
 
                //GET de los productos al carrito
 
-               const carritoItem = {...Storage.ge}
+               const carritoItem = {...Storage.getProductos(id), cantidad: 1}
+
+                //Agregando el producto al carrito
+                carrito = [...carrito, carritoItem]
+
+                //Guardar el carrito en el localstorage
+                Storage.saveCart(carrito)
+
+                //set cart values
+                this.setItemValues(carrito)
+                this.addCarritoItem(carritoItem)
+
+                // Mostrar el carrito
+
             });
         });
     }
+
+    SetItemValues(carrito){
+        let tempTotal = 0;
+        let itemTotal = 0;
+        carrito.map(item => {
+            tempTotal += item.price * item.cantidad;
+            itemTotal += item.cantidad;
+        });
+        carritoTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        itemTotales = innerText = itemTotal
+    }
+
+    addCarritoItem({image, price, title, id}){
+        const div = document.createElement("div")
+        div.classList.add("carrito__item")
+
+        div.innerHTML = `
+        <div class="carrito__item">
+                  <img src=${image} alt=${title}>
+                  <div>
+                    <h3>${title}</h3>
+                    <p class="price">$${price}</p>
+                  </div>
+                  <div>
+                    <span class= "increase" data-id=${id}>
+                      <i class="bx bxs-up-arrow"></i>
+                    </span>
+                    <p class="item__cantidad">1</p>
+                    <span class= "decrease" data-id=${id}>
+                      <i class="bx bxs-down-arrow"></i>
+                    </span>
+                  </div>
+                  <div>
+                    <span class="remove__item" data-id=${id}>
+                      <i class="bx bx-trash"></i>
+                    </span>
+                  </div>
+        `
+        carrito__center.appendChild(div)
+    }
+    show(){
+        carritoDOM.classList.add("show")
+        overlay.classList.add("show")
+    }
+    setAPP(){
+        carrito = Storage.getCart()
+        this.setItemValues(carrito)
+        this.populate(carrito)
+
+        openCarrito.addEventListener("click", this.show)
+        closeCarrito.addEventListener("click", this.hide)
+
+        populate(carrito){
+            carrito.forEach(item => this.addCarritoItem(item))
+        }
+        carritoCenter.addEventListener("click", e =>{
+			const target = e.target.closest("span")
+			const targetElement = target.classList.contains("remove__item");
+			console.log(target)
+			console.log(targetElement)
+			if(!target) return
+			if(targetElement){
+				const id = parseInt(target.dataset.id);
+				this.removeItem(id)
+				carritoCenter.removeChild(target.parentElement.parentElement)
+			}else if(target.classList.contains("increase")){
+				const id = parseInt(target.dataset.id, 10);
+				let tempItem = carrito.find(item => item.id === id);
+				tempItem.cantidad++;
+				Storage.saveCart(carrito)
+				this.setItemValues(carrito)
+				target.nextElementSibling.innerText = tempItem.cantidad
+			}else if(target.classList.contains("decrease")){
+				const id = parseInt(target.dataset.id, 10);
+				let tempItem = carrito.find(item => item.id === id);
+				tempItem.cantidad--;
+
+				if(tempItem.cantidad > 0){
+					Storage.saveCart(carrito);
+					this.setItemValues(carrito);
+					target.previousElementSibling.innerText = tempItem.cantidad;
+				}else{
+					this.removeItem(id);
+					carritoCenter.removeChild(target.parentElement.parentElement)
+				}
+			}
+		});
+	}
+
 }
+    
+
 
 class Storage {
     static saveProduct(obj){
@@ -99,7 +203,9 @@ class Storage {
         const producto = JSON.parse(localStorage.getItem("productos"))
         return producto.find(product => product.id === parseFloat(id, 10))
     }
-
+    static getCart(){
+        return localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
+    }
 }
 
 
